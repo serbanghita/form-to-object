a5d6aa5ceec0e73afcc3f3b7ebf88f56fc59160b
Serban Ghita <serbanghita@gmail.com>
2013-09-27 00:40:34 +0300

    New tests. Still need more to add.

--- tests/tests.js
+++ tests/tests.js
@@ -7,6 +7,9 @@ QUnit.test( 'basic functionality tests', function() {
 	equal( form === false, false, 'Method returns false on an empty DOM element.');
 
 	form = new formToObject('testForm');
-	equal( typeof form === 'object', true, 'Method returns an object on an existent DOM form.' );
+	equal( typeof form === 'object', true, 'Method returns an object on an existent DOM form using DOM id string in the constructor.' );
+
+	form = new formToObject(document.getElementById(testForm));
+	equal( typeof form === 'object', true, 'Method returns an object on an existent DOM form using a DOM object in the constructor.');
 
 });
