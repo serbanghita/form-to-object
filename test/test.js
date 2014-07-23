5d8bcb05c201eb3686a7b9ad784068dd3f721ae5
Serban Ghita <serbanghita@gmail.com>
2014-07-24 00:06:34 +0300

    New tests.

--- test/test.js
+++ test/test.js
@@ -69,6 +69,51 @@
 
 	});
 
+	describe('An HTML form with one input element without name attribute', function () {
+
+		beforeEach(function () {
+				var html = '<form id="newForm">' +
+				'<input type="text" value="4111111111111111">' +
+				'</form>';
+				var $newForm = document.createElement('div');
+				$newForm.id = 'fixtures';
+				$newForm.innerHTML = html;
+				document.body.appendChild($newForm);
+			});
+
+		afterEach(function () {
+			document.body.removeChild(document.getElementById('fixtures'));
+		});
+
+		it('should return an empty object', function(){
+			expect(formToObject('newForm')).toEqual({});
+		});
+
+	});
+
+	describe('An HTML form that contains one field without name attribute', function () {
+
+		beforeEach(function () {
+				var html = '<form id="newForm">' +
+				'<input type="text" name="name" value="Serban">' +
+				'<input type="text" value="4111111111111111">' +
+				'</form>';
+				var $newForm = document.createElement('div');
+				$newForm.id = 'fixtures';
+				$newForm.innerHTML = html;
+				document.body.appendChild($newForm);
+			});
+
+		afterEach(function () {
+			document.body.removeChild(document.getElementById('fixtures'));
+		});
+
+		it('should return the non empty valid keys and values', function(){
+			expect(formToObject('newForm')).toEqual({'name':'Serban'});
+		});
+
+	});
+
 	/**
 	 * Input elements tests.
 	 *
@@ -440,6 +485,123 @@
 		});
 
 	});
+
+	/**
+	 * Textarea elements test.
+	 */
+
+  describe('An HTML form with a textarea element', function(){
+
+		beforeEach(function () {
+				var html = '<form id="newForm">' +
+						'<textarea name="address">Place du Casino, 98000 Monaco</textarea>' +
+						'</form>';
+				var $newForm = document.createElement('div');
+				$newForm.id = 'fixtures';
+				$newForm.innerHTML = html;
+				document.body.appendChild($newForm);
+			});
+
+		afterEach(function () {
+			document.body.removeChild(document.getElementById('fixtures'));
+		});
+
+		it('should return an object', function(){
+			expect(formToObject('newForm')).toEqual({'address':'Place du Casino, 98000 Monaco'});
+		});
+
+  });
+
+  /**
+   * Select elements test.
+   */
+  describe('An HTML form with a select element and one valid selected option', function(){
+
+		beforeEach(function () {
+				var html = '<form id="newForm">' +
+						'<select name="country">' +
+						'<option value="RO">Romania</option>' +
+						'<option value="MC" selected>Monaco</option>' +
+						'<option value="US">United States of America</option>' +
+						'</select>' +
+						'</form>';
+				var $newForm = document.createElement('div');
+				$newForm.id = 'fixtures';
+				$newForm.innerHTML = html;
+				document.body.appendChild($newForm);
+			});
+
+		afterEach(function () {
+			document.body.removeChild(document.getElementById('fixtures'));
+		});
+
+		it('should return an object', function(){
+			expect(formToObject('newForm')).toEqual({'country':'MC'});
+		});
+
+  });
+
+  describe('An HTML form with a select element and no selected options', function(){
+
+		beforeEach(function () {
+				var html = '<form id="newForm">' +
+						'<select name="country">' +
+						'<option value="">--select--</option>' +
+						'<option value="RO">Romania</option>' +
+						'<option value="MC">Monaco</option>' +
+						'<option value="US">United States of America</option>' +
+						'</select>' +
+						'</form>';
+				var $newForm = document.createElement('div');
+				$newForm.id = 'fixtures';
+				$newForm.innerHTML = html;
+				document.body.appendChild($newForm);
+			});
+
+		afterEach(function () {
+			document.body.removeChild(document.getElementById('fixtures'));
+		});
+
+		it('should return the first option value', function(){
+			expect(formToObject('newForm')).toEqual({'country':''});
+		});
+
+  });
+
+  describe('An HTML form with a multiple select element', function(){
+
+		beforeEach(function () {
+				var html = '<form id="newForm">' +
+						'<select name="countries" id="countries" multiple>' +
+						'<option value="">--select--</option>' +
+						'<option value="RO">Romania</option>' +
+						'<option value="MC">Monaco</option>' +
+						'<option value="US">United States of America</option>' +
+						'</select>' +
+						'</form>';
+				var $newForm = document.createElement('div');
+				$newForm.id = 'fixtures';
+				$newForm.innerHTML = html;
+				document.body.appendChild($newForm);
+			});
+
+		afterEach(function () {
+			document.body.removeChild(document.getElementById('fixtures'));
+		});
+
+		it('should return an empty array when no options are selected', function(){
+			expect(formToObject('newForm')).toEqual({'countries':[]});
+		});
+
+		it('should return an array with two elements when two options are selected', function(){
+			var $countries = document.getElementById('countries');
+			$countries.options[1].setAttribute('selected', true);
+			$countries.options[2].setAttribute('selected', true);
+
+			expect(formToObject('newForm')).toEqual({'countries':['RO', 'MC']});
+		});
+
+  });
 
 
 })();
