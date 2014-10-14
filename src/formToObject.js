44ff8bc145e3aa5861c3df36e56e9f972f5d264f
Serban Ghita <serbanghita@gmail.com>
2014-10-14 18:29:13 +0300

    Added processSingleLevelNode and processMultiLevelNode methods. Work in progress.

--- src/formToObject.js
+++ src/formToObject.js
@@ -30,9 +30,9 @@
 
 		// Get last numeric key from an object.
 		function getLastNumericKey(o){
-			return Object.keys(o).filter(function(elem){ 
+			return parseInt(Object.keys(o).filter(function(elem){ 
 				return !isNaN(parseInt(elem,10)); 
-			}).splice(-1)[0];
+			}).splice(-1)[0], 10) || 0;
 		}	
 
 		// Get the real number of properties from an object.
@@ -52,7 +52,28 @@
 
 			return l;
 
-		}			
+		}
+
+
+		// Iteration through arrays and objects. Compatible with IE.
+		function forEach( arr, callback ){
+
+			if([].forEach){
+				return [].forEach.call(arr, callback);
+			}
+
+			var i;
+			for(i in arr){
+				// Using Object.prototype.hasOwnProperty instead of
+				// arr.hasOwnProperty for IE8 compatibility.
+				if( Object.prototype.hasOwnProperty.call(arr,i) ){
+					callback.call(arr, arr[i]);
+				}
+			}
+
+			return;
+
+		}					
 
 		// Constructor
 		function init(options){
@@ -100,23 +121,76 @@
 
 		}
 
-		function processNode(arr, value, result){
+		function processSingleLevelNode($domNode, value, result){
+
+			var key = $domNode.name;
+
+			// We're only interested in the radio that is checked.
+			if( $domNode.nodeName === 'INPUT' &&
+				$domNode.type === 'radio' ) {
+				if( $domNode.checked ){
+					result[key] = value;
+					return value;
+				} else {
+					return;
+				}
+			}
+
+			// Checkboxes are a special case. We have to grab each checked values
+			// and put them into an array.
+			if( $domNode.nodeName === 'INPUT' &&
+				$domNode.type === 'checkbox' ) {
+
+				if( $domNode.checked ){
+					if( !result[key] ){
+						result[key] = [];
+					}
+					return result[key].push( value );
+				} else {
+					return;
+				}
+
+			}
+
+			// Multiple select is a special case.
+			// We have to grab each selected option and put them into an array.
+			if( $domNode.nodeName === 'SELECT' &&
+				$domNode.type === 'select-multiple' ) {
+
+				result[key] = [];
+				var $domNodeChilds = $domNode.querySelectorAll('option[selected]');
+				if( $domNodeChilds ){
+					forEach($domNodeChilds, function(child){
+						result[key].push( child.value );
+					});
+				}
+				return;
+
+			}
+
+			// Fallback. The default one to one assign.
+			result[key] = value;
+
+
+		}
+
+		function processMultiLevelNode(arr, value, result){
 
 			var keyName = arr[0];
 
 			if(arr.length > 1){
 				if( keyName === '[]' ){ 
-					result.push([]);
-					return processNode(arr.splice(1, arr.length), value, result[getLastNumericKey(result)]);
+					result.push({});
+					return processMultiLevelNode(arr.splice(1, arr.length), value, result[getLastNumericKey(result)]);
 				} else {
 
 					if( result[keyName] && getObjLength(result[keyName]) > 0 ) {
 						//result[keyName].push(null);
-						return processNode(arr.splice(1, arr.length), value, result[keyName]); 
+						return processMultiLevelNode(arr.splice(1, arr.length), value, result[keyName]); 
 					} else {
-						result[keyName] = [];				
+						result[keyName] = {};				
 					}	
-					return processNode(arr.splice(1, arr.length), value, result[keyName]);		
+					return processMultiLevelNode(arr.splice(1, arr.length), value, result[keyName]);		
 				}
 				
 			}
@@ -124,7 +198,8 @@
 			// Last key, attach the original value.
 			if(arr.length === 1){
 				if( keyName === '[]' ){
-					result.push(value);
+					//result.push(value);
+					result[getLastNumericKey(result) + 1] = value;
 					return result;
 				} else {
 					result[keyName] = value;
@@ -137,18 +212,26 @@
 		function convertToObj(){
 
 			var i = 0, 
-				objKeyNames, 
-				result = [];
+				objKeyNames,
+				$domNode,
+				result = {};
 
 			for(i = 0; i < $formElements.length; i++){
+				
+				$domNode = $formElements[i];
+
 				// Ignore the element if the 'name' attribute is empty.
 				// Ignore the 'disabled' elements.
-				if( $formElements[i].name && !$formElements[i].disabled ) {
-					objKeyNames = $formElements[i].name.match( keyRegex );
-					if( objKeyNames.length > 0 ){
-						processNode(objKeyNames, $formElements[i].value, result);
+				if( $domNode.name && !$domNode.disabled ) {
+					objKeyNames = $domNode.name.match( keyRegex );
+					if( objKeyNames.length === 1 ) {
+						processSingleLevelNode($domNode, $domNode.value, result);
+					}
+					if( objKeyNames.length > 1 ){												
+						processMultiLevelNode(objKeyNames, $domNode.value, result);
 					}
 				}
+
 			}
 
 			return result;
