fa8f7de1383a85096911373460fb16a53d722237
Serban Ghita <serbanghita@gmail.com>
2016-01-06 23:24:23 +0200

    Applied JSCS standards.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,67 +1,68 @@
 module.exports = function(grunt) {
 
-  'use strict';
+    'use strict';
 
-  // window.serban = process.env.PWD;
+    // window.serban = process.env.PWD;
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> ' +
-                '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-        options: {
-          jshintrc: true
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> ' +
+                        '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js',
+            },
         },
-        all: ['src/<%= pkg.name %>.js'],
-        grunt: {
-          src: 'Gruntfile.js'
-        }
-      },
-      jasmine: {
-          'default': {
-            src: 'src/*.js',
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            all: ['src/<%= pkg.name %>.js'],
+            grunt: {
+                src: 'Gruntfile.js'
+            },
+        },
+        jasmine: {
+          default: {
+              src: 'src/*.js',
               options: {
-              specs: [
-                'test/testInput.js',
-                'test/testInputFile.js',
-                'test/testTextarea.js',
-                'test/testSelect.js',
-                'test/testCheckbox.js',
-                'test/testRadio.js',
-                'test/testUnexpected.js',
-                'test/testExceptions.js',
-                'test/testComplexForms.js'
-              ],
-              vendor: ['vendor/jquery/jquery.js', 'vendor/jquery/jasmine-jquery.js'],
-              outfile: 'test/SpecRunner.html',
-              keepRunner: true,
-              '--web-security': 'no'
-            }
+                  specs: [
+                    'test/testInput.js',
+                    'test/testInputFile.js',
+                    'test/testTextarea.js',
+                    'test/testSelect.js',
+                    'test/testCheckbox.js',
+                    'test/testRadio.js',
+                    'test/testUnexpected.js',
+                    'test/testExceptions.js',
+                    'test/testComplexForms.js',
+                  ],
+                  vendor: [
+                    'vendor/jquery/jquery.js',
+                    'vendor/jquery/jasmine-jquery.js',
+                  ],
+                  outfile: 'test/SpecRunner.html',
+                  keepRunner: true,
+                  '--web-security': 'no',
+              },
           }
-      }
+      },
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('grunt-karma');
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-  //grunt.loadNpmTasks('grunt-karma');
-
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-
-  // Default task(s).
-  grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']); // 'jshint',
+    // Default task(s).
+    grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']); // 'jshint',
 
 };
