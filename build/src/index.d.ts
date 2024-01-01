761be52f41d8e93fd8b58eb98460616e8ffbade1
Serban Ghita <serbanghita@gmail.com>
2024-01-02 00:41:58 +0200

    Remove generated ts files

--- build/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { IFormToObjectOptions } from "./types";
-export default function formToObject<T>(selector: HTMLFormElement | string, options?: IFormToObjectOptions): T | undefined;
--- build/test/helpers.d.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from '@jest/globals';
-import { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
-export declare function readIntegrationFixture(relativePath: string): string;
-declare module 'expect' {
-    interface Matchers<R = void> extends TestingLibraryMatchers<typeof expect.stringContaining, R> {
-    }
-}
